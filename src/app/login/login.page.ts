7a0b4f87c4cc0ea64e2857a8d55f093d0559c89c
asamy882 <36104073+asamy882@users.noreply.github.com>
2020-04-12 16:17:07 +0200

    companyList as static

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -25,6 +25,18 @@ export class LoginPage implements OnInit {
   constructor(private fb: FormBuilder, private service: AuthService, private router: Router, public platform: Platform,
               private toastController: ToastController, private languageService: LanguageService,public loadingService: LoadingService,
               private readonly translate: TranslateService, public events: Events, private zone: NgZone) {
+    this.companyList = [{
+                Code: '43350743-210d-49ad-817b-4c74a9f5d507',
+                Name: 'QARETAIL'
+            },
+            {
+                Code: '9681bd11-5f44-47c9-b940-7aecb7254587',
+                Name: 'Test'
+            },
+            {
+                Code: 'a93ceba6-d746-4cc1-8c35-d3c819bbd25a',
+                Name: 'QUATTRO'
+            }] ;
     this.authForm = fb.group({
       username: [null, Validators.compose([Validators.required])],
       company: [null, Validators.compose([Validators.required])],
@@ -49,8 +61,8 @@ export class LoginPage implements OnInit {
 
   onEnter(){
     this.platform.ready().then( () => {
-      console.log('platform ready fired  ');
-      this.translate.use(this.languageService.currentLang);
+    console.log('platform ready fired  ');
+    this.translate.use(this.languageService.currentLang);
     const companyId = localStorage.getItem('companyId');
     const username = localStorage.getItem('username');
     const password = localStorage.getItem('password');
@@ -58,12 +70,12 @@ export class LoginPage implements OnInit {
       this.keepMeLoginFun(companyId, username, password);
      // this.navCtrl.setRoot('HomePage');
     } else {
-      this.loadingService.present();
-        console.log('get companies ')
-        this.service.getCompanies().subscribe(res => {
+      /*this.loadingService.present();
+         console.log('get companies ')
+         this.service.getCompanies().subscribe(res => {
           this.companyList = res.Items;
           this.loadingService.dismiss();
-        });
+        }); */
     }
     })
   }
